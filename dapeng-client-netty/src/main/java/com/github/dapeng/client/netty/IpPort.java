8d3f36e3b7b6a510137c0d242e7d656b0091b593
大蛋散 <439674061@qq.com>
2018-01-10 15:04:17 +0800

    解决客户端连接状态错误的问题

--- dapeng-client-netty/src/main/java/com/github/dapeng/client/netty/IpPort.java
+++ dapeng-client-netty/src/main/java/com/github/dapeng/client/netty/IpPort.java
@@ -11,4 +11,32 @@ public class IpPort {
         this.ip = ip;
         this.port = port;
     }
+
+    @Override
+    public String toString() {
+        return this.ip + ": " + this.port;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        } else if (this == obj) {
+            return true;
+        } else if (obj instanceof IpPort) {
+            IpPort ipPort = (IpPort) obj;
+            if (ip.equals(ipPort.ip) && port == ipPort.port) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return (this.port + this.ip).hashCode();
+    }
 }
