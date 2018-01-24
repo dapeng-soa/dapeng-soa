9e4372c52e1efe2d9dbde64f9545cce41275951b
jackliang <liangbinbin@iplas.com.cn>
2018-01-24 17:12:22 +0800

    优化代码.

--- dapeng-client-netty/src/main/java/com/github/dapeng/client/netty/SubPool.java
+++ dapeng-client-netty/src/main/java/com/github/dapeng/client/netty/SubPool.java
@@ -3,10 +3,14 @@ package com.github.dapeng.client.netty;
 import com.github.dapeng.core.SoaConnection;
 import com.github.dapeng.util.SoaJsonConnectionImpl;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Created by lihuimin on 2017/12/25.
  */
 public class SubPool {
+
+    private final ReentrantLock lock = new ReentrantLock();
 
     final String ip;
     final int port;
@@ -27,16 +31,23 @@ public class SubPool {
      */
     public SoaConnection getConnection(ConnectionType connectionType) {
 
-        if (connectionType == ConnectionType.Json) {
-            if (jsonConnection == null){
-                jsonConnection = new SoaJsonConnectionImpl(ip,port);
+        try {
+            if (connectionType == ConnectionType.Json) {
+                lock.lock();
+                if (jsonConnection == null){
+                    jsonConnection = new SoaJsonConnectionImpl(ip,port);
+                }
+                return jsonConnection;
+            } else {
+                lock.lock();
+                if (commonConnection == null) {
+                    commonConnection = new SoaConnectionImpl(ip,port);
+                }
+                return commonConnection;
             }
-            return jsonConnection;
-        } else {
-            if (commonConnection == null) {
-                commonConnection = new SoaConnectionImpl(ip,port);
-            }
-            return commonConnection;
+        } finally {
+            lock.unlock();
         }
+
     }
 }
