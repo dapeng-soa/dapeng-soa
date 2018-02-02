60cfb65aa23f089f3ae6dbe65dab26c408da09a3
StruggleYang <yq1724555319@gmail.com>
2018-02-02 11:06:51 +0800

    qps 监控测试通过

---
+++ dapeng-monitor-filter/src/main/java/com/github/dapeng/monitor/domain/MonitorProperties.java
@@ -0,0 +1,39 @@
+package com.github.dapeng.monitor.domain;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * author with struy.
+ * Create by 2018/2/1 19:07
+ * email :yq1724555319@gmail.com
+ */
+
+public class MonitorProperties {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MonitorProperties.class);
+
+    private static final Properties MONITOR_PROPERTIES = new Properties();
+    private static final String KEY_MONITOR_INFLUXDB_DATABASE = "monitor.influxdb.database";
+    private static final String KEY_MONITOR_QPS_PERIOD = "monitor.qps.period";
+    private static final String KEY_MONITOR_SERVICE_PROCESS_PERIOD = "monitor.service.process.period";
+
+    public static String MONITOR_INFLUXDB_DATABASE = get(KEY_MONITOR_INFLUXDB_DATABASE,"dapengState");
+    public static Integer MONITOR_QPS_PERIOD = Integer.valueOf(get(KEY_MONITOR_QPS_PERIOD,"5"));
+    public static Integer MONITOR_SERVICE_PROCESS_PERIOD = Integer.valueOf(get(KEY_MONITOR_SERVICE_PROCESS_PERIOD,"60"));
+
+    private static String get(String key, String defaultValue) {
+        try {
+            InputStream in = MonitorProperties.class.getClassLoader().getResourceAsStream("config_monitor.properties");
+            MONITOR_PROPERTIES.load(in);
+        } catch (IOException e) {
+            LOGGER.error("error reading file config_monitor.properties");
+            LOGGER.error(e.getMessage(), e);
+        }
+        return MONITOR_PROPERTIES.getProperty(key, defaultValue);
+
+    }
+}
