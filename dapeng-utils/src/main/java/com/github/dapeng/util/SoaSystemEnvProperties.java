c8a9a449335b61205e6f9b2a80cbc21e80930a38
StruggleYang <1724555319@qq.com>
2018-02-06 01:20:39 +0800

    fix code ,add properties

--- dapeng-utils/src/main/java/com/github/dapeng/util/SoaSystemEnvProperties.java
+++ dapeng-utils/src/main/java/com/github/dapeng/util/SoaSystemEnvProperties.java
@@ -16,6 +16,14 @@ public class SoaSystemEnvProperties {
     private static final String KEY_SOA_APIDOC_PORT = "soa.apidoc.port";
     private static final String KEY_SOA_KAFKA_HOST = "soa.kafka.host";
 
+        private static final String KEY_SOA_MONITOR_INFLUXDB_DATABASE = "soa.monitor.infulxdb.database";
+    private static final String KEY_SOA_MONITOR_QPS_PERIOD = "soa.monitor.qps.period";
+    private static final String KEY_SOA_MONITOR_SERVICE_PROCESS_PERIOD = "soa.monitor.service.period";
+
+    public static String SOA_MONITOR_INFLUXDB_DATABASE = get(KEY_SOA_MONITOR_INFLUXDB_DATABASE,"dapengState");
+    public static Integer SOA_MONITOR_QPS_PERIOD = Integer.valueOf(get(KEY_SOA_MONITOR_QPS_PERIOD,"5"));
+    public static Integer SOA_MONITOR_SERVICE_PROCESS_PERIOD = Integer.valueOf(get(KEY_SOA_MONITOR_SERVICE_PROCESS_PERIOD,"60"));
+
     /**
      * 可指定主从竞选master
      */
