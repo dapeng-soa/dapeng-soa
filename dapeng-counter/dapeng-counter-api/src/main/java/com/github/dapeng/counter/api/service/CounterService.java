20e0da0a196fc2e7212f6da5e16d6d3d73174240
StruggleYang <yq1724555319@gmail.com>
2018-02-07 00:52:27 +0800

    include dapeng-counter

---
+++ dapeng-counter/dapeng-counter-api/src/main/java/com/github/dapeng/counter/api/service/CounterService.java
@@ -0,0 +1,17 @@
+package com.github.dapeng.counter.api.service;
+
+/**
+ * author with struy.
+ * Create by 2018/2/7 00:38
+ * email :yq1724555319@gmail.com
+ */
+
+public interface CounterService {
+
+    void submitPoint();
+
+    void submitPoints();
+
+    void queryPoints();
+
+}
---
+++ dapeng-counter/dapeng-counter-service/src/main/java/com/github/dapeng/counter/service/CounterServiceImpl.java
@@ -0,0 +1,26 @@
+package com.github.dapeng.counter.service;
+
+import com.github.dapeng.counter.api.service.CounterService;
+
+/**
+ * author with struy.
+ * Create by 2018/2/7 00:43
+ * email :yq1724555319@gmail.com
+ */
+
+public class CounterServiceImpl implements CounterService {
+    @Override
+    public void submitPoint() {
+
+    }
+
+    @Override
+    public void submitPoints() {
+
+    }
+
+    @Override
+    public void queryPoints() {
+
+    }
+}
