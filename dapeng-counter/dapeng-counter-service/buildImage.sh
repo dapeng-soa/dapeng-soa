65bdeed1003bfa901b320c1f69a6b8a7d2a4b783
Ever <439674061@qq.com>
2018-02-12 14:47:42 +0800

    2.0.1-SNAPSHOT

--- dapeng-counter/dapeng-counter-service/buildImage.sh
+++ dapeng-counter/dapeng-counter-service/buildImage.sh
@@ -2,5 +2,5 @@
 
 mvn clean package
 mv target/dapeng-counter-service docker/
-docker build -t dapengsoa/counter-service:2.0.0 docker/
+docker build -t dapengsoa/counter-service:2.0.1-SNAPSHOT docker/
 rm -rf docker/dapeng-counter-service
