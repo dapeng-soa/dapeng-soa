9242caf818a67c6d20ef19b48a6b3b74f4dea371
leihuazhe <295482300@qq.com>
2018-02-13 12:06:27 +0800

    DDD消息序列化与kafka生产者代码

---
+++ dapeng-message/dapeng-message-kafka/src/main/java/com/github/dapeng/message/consumer/kafka/serializer/KafkaMessageProcessor.java
@@ -0,0 +1,45 @@
+package com.github.dapeng.message.consumer.kafka.serializer;
+
+import com.github.dapeng.core.BeanSerializer;
+import com.github.dapeng.message.consumer.kafka.MessageInfo;
+import com.github.dapeng.org.apache.thrift.TException;
+import com.github.dapeng.org.apache.thrift.protocol.TCompactProtocol;
+/**
+ * 描述: kafka 消息 解析器
+ *
+ * @author maple.lei
+ * @date 2018年02月13日 上午11:39
+ */
+public class KafkaMessageProcessor<T> {
+
+    private BeanSerializer<T> beanSerializer;
+
+    public KafkaMessageProcessor(BeanSerializer<T> beanSerializer) {
+        this.beanSerializer = beanSerializer;
+    }
+
+    /**
+     * decode kafka message
+     *
+     * @return
+     */
+    public MessageInfo<T> parseMessage(byte[] bytes) throws TException {
+        TKafkaTransport kafkaTransport = new TKafkaTransport(bytes, TKafkaTransport.Type.Read);
+        TCompactProtocol protocol = new TCompactProtocol(kafkaTransport);
+        String eventType = kafkaTransport.getEventType();
+        T event = beanSerializer.read(protocol);
+        return new MessageInfo<>(eventType, event);
+    }
+
+    public byte[] buildMessageByte(T event) throws TException {
+        byte[] bytes = new byte[8192];
+        TKafkaTransport kafkaTransport = new TKafkaTransport(bytes, TKafkaTransport.Type.Write);
+        TCompactProtocol protocol = new TCompactProtocol(kafkaTransport);
+        String eventType = event.getClass().getName();
+        kafkaTransport.setEventType(eventType);
+        beanSerializer.write(event, protocol);
+        kafkaTransport.flush();
+        bytes = kafkaTransport.getByteBuf();
+        return bytes;
+    }
+}
