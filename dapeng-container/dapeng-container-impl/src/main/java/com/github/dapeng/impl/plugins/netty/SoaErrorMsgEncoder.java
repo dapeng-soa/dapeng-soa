aac80648b97e1d7ca8daede55e48cc662b648341
Ever <439674061@qq.com>
2018-02-19 12:42:22 +0800

    Handle the exception

--- dapeng-container/dapeng-container-impl/src/main/java/com/github/dapeng/impl/plugins/netty/SoaErrorMsgEncoder.java
+++ dapeng-container/dapeng-container-impl/src/main/java/com/github/dapeng/impl/plugins/netty/SoaErrorMsgEncoder.java
@@ -3,7 +3,6 @@ package com.github.dapeng.impl.plugins.netty;
 import com.github.dapeng.api.Container;
 import com.github.dapeng.client.netty.TSoaTransport;
 import com.github.dapeng.core.*;
-import com.github.dapeng.core.filter.FilterContext;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
@@ -11,17 +10,13 @@ import io.netty.handler.codec.MessageToByteEncoder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Optional;
-
-import static com.github.dapeng.util.SoaSystemEnvProperties.SOA_NORMAL_RESP_CODE;
-
 /**
  * 错误返回的消息编码器
  *
  * @author Ever
  */
 @ChannelHandler.Sharable
-public class SoaErrorMsgEncoder extends MessageToByteEncoder<SoaException> {
+public class SoaErrorMsgEncoder extends MessageToByteEncoder<TransactionContext> {
     private static final Logger LOGGER = LoggerFactory.getLogger(SoaErrorMsgEncoder.class);
 
     private final Container container;
@@ -31,19 +26,15 @@ public class SoaErrorMsgEncoder extends MessageToByteEncoder<SoaException> {
     }
 
     @Override
-    protected void encode(ChannelHandlerContext channelHandlerContext, SoaException soaException, ByteBuf out) throws Exception {
-        TransactionContext transactionContext = TransactionContext.Factory.getCurrentInstance();
-        SoaHeader soaHeader = transactionContext.getHeader();
-
+    protected void encode(ChannelHandlerContext channelHandlerContext, TransactionContext transactionContext, ByteBuf out) throws Exception {
         ByteBuf outputBuf = channelHandlerContext.alloc().buffer(8192);
         TSoaTransport transport = new TSoaTransport(outputBuf);
         SoaMessageProcessor messageProcessor = new SoaMessageProcessor(transport);
 
+        SoaHeader soaHeader = transactionContext.getHeader();
         Application application = container.getApplication(new ProcessorKey(soaHeader.getServiceName(), soaHeader.getVersionName()));
-
+        SoaException soaException = transactionContext.getSoaException();
         try {
-            soaHeader.setRespCode(Optional.ofNullable(soaException.getCode()));
-            soaHeader.setRespMessage(Optional.ofNullable(soaException.getMsg()));
             messageProcessor.writeHeader(transactionContext);
             messageProcessor.writeMessageEnd();
 
