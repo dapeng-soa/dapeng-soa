367a84d31c4f5c0a29a8796450ad2ebcbf3aaee1
Ever <439674061@qq.com>
2018-02-19 10:49:45 +0800

    Handle the exception

--- dapeng-container/dapeng-container-impl/src/main/java/com/github/dapeng/impl/plugins/netty/SoaNormalMsgEncoder.java
+++ dapeng-container/dapeng-container-impl/src/main/java/com/github/dapeng/impl/plugins/netty/SoaNormalMsgEncoder.java
@@ -1,9 +1,11 @@
 package com.github.dapeng.impl.plugins.netty;
 
+import com.github.dapeng.api.Container;
 import com.github.dapeng.client.netty.TSoaTransport;
 import com.github.dapeng.core.*;
 import com.github.dapeng.core.filter.FilterContext;
 import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
 import org.slf4j.Logger;
@@ -18,8 +20,13 @@ import static com.github.dapeng.util.SoaSystemEnvProperties.SOA_NORMAL_RESP_CODE
  *
  * @author Ever
  */
+@ChannelHandler.Sharable
 public class SoaNormalMsgEncoder extends MessageToByteEncoder<FilterContext> {
-    private static final Logger LOGGER = LoggerFactory.getLogger(SoaNormalMsgEncoder.class);
+    private final Container container;
+
+    SoaNormalMsgEncoder(Container container) {
+        this.container = container;
+    }
 
     @Override
     protected void encode(ChannelHandlerContext channelHandlerContext, FilterContext ctx, ByteBuf out) throws Exception {
@@ -49,10 +56,19 @@ public class SoaNormalMsgEncoder extends MessageToByteEncoder<FilterContext> {
 
                 assert (outputBuf.refCnt() == 1);
                 channelHandlerContext.writeAndFlush(outputBuf);
+
+                Application application = container.getApplication(new ProcessorKey(soaHeader.getServiceName(), soaHeader.getVersionName()));
+
+                application.info(this.getClass(),
+                        soaHeader.getServiceName()
+                                + ":" + soaHeader.getVersionName()
+                                + ":" + soaHeader.getMethodName()
+                                + " operatorId:" + soaHeader.getOperatorId()
+                                + " operatorName:" + soaHeader.getOperatorName()
+                                + " response sent");
+
             }
         } catch (Throwable e) {
-            LOGGER.error(e.getMessage(), e);
-
             channelHandlerContext.writeAndFlush(new SoaException(SoaCode.UnKnown, e.getMessage()));
 
             if (outputBuf != null) {
