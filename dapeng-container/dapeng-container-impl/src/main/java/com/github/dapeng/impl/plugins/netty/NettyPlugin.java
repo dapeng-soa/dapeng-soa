cb8d49a0c03d218bf4894c0b4d475fffb14b73eb
Ever <439674061@qq.com>
2018-02-20 01:45:03 +0800

    add comment

--- dapeng-container/dapeng-container-impl/src/main/java/com/github/dapeng/impl/plugins/netty/NettyPlugin.java
+++ dapeng-container/dapeng-container-impl/src/main/java/com/github/dapeng/impl/plugins/netty/NettyPlugin.java
@@ -69,9 +69,9 @@ public class NettyPlugin implements AppListener, Plugin {
                                             new SoaDecoder(), //粘包和断包处理
                                             new SoaIdleHandler(),  //心跳处理
                                             new SoaMsgDecoder(container),//请求解码器
-                                            new SoaErrorMsgEncoder(container),//错误响应编码器
-                                            new SoaFilterMsgEncoder(container),//正常响应编码器
-                                            new SoaServerHandler(container));  //业务处理处理
+                                            new SoaErrorMsgEncoder(container),//错误响应消息编码器
+                                            new SoaFilterMsgEncoder(container),//经Filter处理后的响应消息的编码器
+                                            new SoaServerHandler(container));  //业务处理器
                                 }
                             })
                             .option(ChannelOption.SO_BACKLOG, 1024)
