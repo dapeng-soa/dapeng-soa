59e548c0f416c882be43b5307e4538c66c13bad8
Ever <439674061@qq.com>
2018-02-22 08:31:23 +0700

    remove useless imports

--- dapeng-container/dapeng-container-impl/src/main/java/com/github/dapeng/impl/plugins/netty/SoaFilterMsgEncoder.java
+++ dapeng-container/dapeng-container-impl/src/main/java/com/github/dapeng/impl/plugins/netty/SoaFilterMsgEncoder.java
@@ -9,8 +9,6 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Optional;
 
