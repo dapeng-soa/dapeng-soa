a900ab9f897669eb8eb6be70fc17270b04b20270
StruggleYang <yq1724555319@gmail.com>
2018-04-08 21:52:42 +0800

    增加netty连接数监控

--- dapeng-container/dapeng-container-impl/src/main/java/com/github/dapeng/impl/plugins/monitor/mbean/ContainerRuntimeInfoMBean.java
+++ dapeng-container/dapeng-container-impl/src/main/java/com/github/dapeng/impl/plugins/monitor/mbean/ContainerRuntimeInfoMBean.java
@@ -4,65 +4,56 @@ package com.github.dapeng.impl.plugins.monitor.mbean;
  * @author ever
  */
 public interface ContainerRuntimeInfoMBean {
-    //attribute？
-
-    //op？
-
-    /**
-     * 停止容器
-     * @return
-     */
-    public boolean stopContainer();
 
     /**
      * 禁用/开启监控
      * @param enable
      * @return
      */
-    public boolean enableMonitor(boolean enable);
+    boolean enableMonitor(boolean enable);
 
     /**
      * 获取日志级别
      * @return
      */
-    public String getLoggerLevel();
+    String getLoggerLevel();
 
     /**
      * 修改日志级别
      * @return
      */
-    public String setLoggerLevel(String level);
+    String setLoggerLevel(String level);
 
     /**
      * 获取业务线程池情况
      * @return
      */
-    public String getTheardPoolStatus();
+    String getTheardPoolStatus();
 
     /**
      * 获取服务信息(包含容器信息)
      * @return
      */
-    public String getSerivceBasicInfo();
+    String getSerivceBasicInfo();
 
     /**
      * 获取当前运行服务流量信息
      * @return
      */
-    public String getServiceFlow();
+    String getServiceFlow();
 
     /**
      * 获取当前服务调用信息
      * @return
      */
-    public String getServiceInvoke();
+    String getServiceInvoke();
 
     /**
      * 获取当前运行服务流量信息 (指定条数)
      * @param count 获取数据条数
      * @return
      */
-    public String getServiceFlow(int count);
+    String getServiceFlow(int count);
 
     /**
      * 获取当前服务调用信息(指定条数)
@@ -70,7 +61,12 @@ public interface ContainerRuntimeInfoMBean {
      * @param methodName 方法名字
      * @return
      */
-    public String getServiceInvoke(int count,String methodName);
+    String getServiceInvoke(int count, String methodName);
 
+    /**
+     * 获取Netty连接数信息
+     * @return
+     */
+    String getNettyChannelStatus();
 
 }
