2cbb8259eaa73f0db159c548f9cfc98c0d75a329
Ever <439674061@qq.com>
2018-04-10 16:41:42 +0800

    update to 2.0.1

--- dapeng-counter/dapeng-counter-service/buildImage.sh
+++ dapeng-counter/dapeng-counter-service/buildImage.sh
@@ -2,5 +2,5 @@
 
 mvn clean package
 mv target/dapeng-counter-service docker/
-docker build -t dapengsoa/counter-service:2.0.1-SNAPSHOT docker/
+docker build -t dapengsoa/counter-service:2.0.1 docker/
 rm -rf docker/dapeng-counter-service
