f71f646d996d62f8c0a4c88230c040825593a8c8
lihuimin <lihuimin@iplas.com.cn>
2018-04-13 15:46:30 +0800

    加上simple log

---
+++ dapeng-container/dapeng-container-impl/src/main/java/com/github/dapeng/impl/filters/SimpleLogFilter.java
@@ -0,0 +1,47 @@
+package com.github.dapeng.impl.filters;
+
+import com.github.dapeng.core.SoaException;
+import com.github.dapeng.core.SoaHeader;
+import com.github.dapeng.core.TransactionContext;
+import com.github.dapeng.core.filter.ContainerFilter;
+import com.github.dapeng.core.filter.FilterChain;
+import com.github.dapeng.core.filter.FilterContext;
+import io.netty.channel.ChannelHandlerContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SimpleLogFilter implements ContainerFilter{
+
+    private static final Logger SIMPLE_LOGGER = LoggerFactory.getLogger("container.simple.log");
+
+    @Override
+    public void onEntry(FilterContext ctx, FilterChain next) throws SoaException {
+
+    }
+
+    @Override
+    public void onExit(FilterContext ctx, FilterChain prev) throws SoaException {
+
+        ChannelHandlerContext channelHandlerContext = (ChannelHandlerContext) ctx.getAttribute("channelHandlerContext");
+        TransactionContext context = (TransactionContext) ctx.getAttribute("context");
+        SoaHeader soaHeader = context.getHeader();
+        long waitingTime = (long)ctx.getAttribute("waitingTime");
+        long startTime = (long)ctx.getAttribute("startTime");
+        Integer requestFlow = (Integer)ctx.getAttribute("requestFlow");
+
+        StringBuilder builder = new StringBuilder("DONE")
+                .append(" ").append(channelHandlerContext.channel().remoteAddress())
+                .append(" ").append(channelHandlerContext.channel().localAddress())
+                .append(" ").append(context.getSeqid())
+                .append(" ").append(soaHeader.getServiceName()).append(".").append(soaHeader.getMethodName()).append(":").append(soaHeader.getVersionName())
+                .append(" ").append(soaHeader.getRespCode())
+                .append(" ").append(soaHeader.getRespMessage())
+                .append(" ").append(requestFlow)
+                .append(" ").append(0)
+                .append(" ").append(waitingTime).append("ms")
+                .append(" ").append(System.currentTimeMillis() - startTime).append("ms");
+
+        SIMPLE_LOGGER.info(builder.toString());
+
+    }
+}
