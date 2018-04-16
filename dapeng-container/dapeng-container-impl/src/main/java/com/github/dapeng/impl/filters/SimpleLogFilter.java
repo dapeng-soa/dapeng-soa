661ebf87015d6abe4b19cc602b52f8edcac141f0
lihuimin <lihuimin@iplas.com.cn>
2018-04-16 15:54:44 +0800

    统计日志filter修改

--- dapeng-container/dapeng-container-impl/src/main/java/com/github/dapeng/impl/filters/SimpleLogFilter.java
+++ dapeng-container/dapeng-container-impl/src/main/java/com/github/dapeng/impl/filters/SimpleLogFilter.java
@@ -16,32 +16,39 @@ public class SimpleLogFilter implements ContainerFilter{
 
     @Override
     public void onEntry(FilterContext ctx, FilterChain next) throws SoaException {
-
+        next.onEntry(ctx);
     }
 
     @Override
     public void onExit(FilterContext ctx, FilterChain prev) throws SoaException {
 
-        ChannelHandlerContext channelHandlerContext = (ChannelHandlerContext) ctx.getAttribute("channelHandlerContext");
-        TransactionContext context = (TransactionContext) ctx.getAttribute("context");
-        SoaHeader soaHeader = context.getHeader();
-        long waitingTime = (long)ctx.getAttribute("waitingTime");
-        long startTime = (long)ctx.getAttribute("startTime");
-        Integer requestFlow = (Integer)ctx.getAttribute("requestFlow");
+        try {
 
-        StringBuilder builder = new StringBuilder("DONE")
-                .append(" ").append(channelHandlerContext.channel().remoteAddress())
-                .append(" ").append(channelHandlerContext.channel().localAddress())
-                .append(" ").append(context.getSeqid())
-                .append(" ").append(soaHeader.getServiceName()).append(".").append(soaHeader.getMethodName()).append(":").append(soaHeader.getVersionName())
-                .append(" ").append(soaHeader.getRespCode())
-                .append(" ").append(soaHeader.getRespMessage())
-                .append(" ").append(requestFlow)
-                .append(" ").append(0)
-                .append(" ").append(waitingTime).append("ms")
-                .append(" ").append(System.currentTimeMillis() - startTime).append("ms");
+            ChannelHandlerContext channelHandlerContext = (ChannelHandlerContext) ctx.getAttribute("channelHandlerContext");
+            TransactionContext context = (TransactionContext) ctx.getAttribute("context");
+            SoaHeader soaHeader = context.getHeader();
+            long waitingTime = (long) ctx.getAttribute("waitingTime");
+            long startTime = (long) ctx.getAttribute("startTime");
+            Integer requestFlow = (Integer) ctx.getAttribute("requestFlow");
 
-        SIMPLE_LOGGER.info(builder.toString());
+            StringBuilder builder = new StringBuilder("DONE")
+                    .append(" ").append(channelHandlerContext.channel().remoteAddress())
+                    .append(" ").append(channelHandlerContext.channel().localAddress())
+                    .append(" ").append(context.getSeqid())
+                    .append(" ").append(soaHeader.getServiceName()).append(".").append(soaHeader.getMethodName()).append(":").append(soaHeader.getVersionName())
+                    .append(" ").append(soaHeader.getRespCode())
+                    .append(" ").append(soaHeader.getRespMessage())
+                    .append(" ").append(requestFlow)
+                    .append(" ").append(0)
+                    .append(" ").append(waitingTime).append("ms")
+                    .append(" ").append(System.currentTimeMillis() - startTime).append("ms");
+
+            SIMPLE_LOGGER.info(builder.toString());
+        }catch (Exception e){
+            SIMPLE_LOGGER.error(e.getMessage(),e);
+        }
+
+        prev.onExit(ctx);
 
     }
 }
