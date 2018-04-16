06bb717d6db1cbf08dd741f2ed1d5c533363878a
StruggleYang <yq1724555319@gmail.com>
2018-04-16 22:05:37 +0800

    修复一个拼写

--- dapeng-container/dapeng-container-impl/src/main/java/com/github/dapeng/impl/plugins/monitor/config/MonitorFilterProperties.java
+++ dapeng-container/dapeng-container-impl/src/main/java/com/github/dapeng/impl/plugins/monitor/config/MonitorFilterProperties.java
@@ -4,7 +4,7 @@ package com.github.dapeng.impl.plugins.monitor.config;
 import com.github.dapeng.core.helper.SoaSystemEnvProperties;
 
 public class MonitorFilterProperties extends SoaSystemEnvProperties {
-    private static final String KEY_SOA_MONITOR_INFLUXDB_DATABASE = "soa.monitor.infulxdb.database";
+    private static final String KEY_SOA_MONITOR_INFLUXDB_DATABASE = "soa.monitor.influxdb.database";
     private static final String KEY_SOA_MONITOR_SERVICE_PROCESS_PERIOD = "soa.monitor.service.period";
 
     public static String SOA_MONITOR_INFLUXDB_DATABASE = get(KEY_SOA_MONITOR_INFLUXDB_DATABASE, "dapengState");
