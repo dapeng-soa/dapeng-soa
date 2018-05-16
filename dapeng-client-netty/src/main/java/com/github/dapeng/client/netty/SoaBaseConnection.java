9dab1ff54ef4f1fac567bb908a27f098fa6434b8
jack <18588733858@163.com>
2018-05-16 16:11:21 +0800

    enhance error log info

--- dapeng-client-netty/src/main/java/com/github/dapeng/client/netty/SoaBaseConnection.java
+++ dapeng-client-netty/src/main/java/com/github/dapeng/client/netty/SoaBaseConnection.java
@@ -310,7 +310,15 @@ public abstract class SoaBaseConnection implements SoaConnection {
                 return new Result<>(resp, null);
             } else {
                 //if exception occur, print the request for trace.
-                LOGGER.info(" error request detail: " + request);
+                String debugLog = "request[seqId:" + invocationContext.seqId() + "]:"
+                        + "service[" + respHeader.getServiceName()
+                        + "]:version[" + respHeader.getVersionName()
+                        + "]:method[" + respHeader.getMethodName() + "]"
+                        + (respHeader.getOperatorId().isPresent() ? " operatorId:" + respHeader.getOperatorId().get() : "") + " "
+                        + (respHeader.getUserId().isPresent() ? " userId:" + respHeader.getUserId().get() : "") + " "
+                        + (respHeader.getCallerIp().isPresent() ? " callerIp: " + respHeader.getCallerIp().get() : "")
+                        + " request detail: " + request;
+                LOGGER.info(" error request detail: " + debugLog);
 
                 return new Result<>(null, new SoaException(
                         (respHeader.getRespCode().isPresent()) ? respHeader.getRespCode().get() : SoaCode.UnKnown.getCode(),
