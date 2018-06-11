702e5cbba98ae33b2a267c1df9fa8c821db5d253
huyongjie <2573105190@qq.com>
2018-06-11 13:16:06 +0800

    1.add schedule sessionTid 2.fix schedule log output to scheduled-task.xxxx-xx-xx.log

--- dapeng-container/dapeng-container-impl/src/main/java/com/github/dapeng/impl/plugins/ScheduledJob.java
+++ dapeng-container/dapeng-container-impl/src/main/java/com/github/dapeng/impl/plugins/ScheduledJob.java
@@ -1,15 +1,19 @@
 package com.github.dapeng.impl.plugins;
 
 import com.github.dapeng.api.ContainerFactory;
+import com.github.dapeng.core.InvocationContext;
+import com.github.dapeng.core.InvocationContextImpl;
 import com.github.dapeng.core.ProcessorKey;
 import com.github.dapeng.core.definition.SoaFunctionDefinition;
 import com.github.dapeng.core.definition.SoaServiceDefinition;
+import com.github.dapeng.core.helper.DapengUtil;
+import com.github.dapeng.core.helper.SoaSystemEnvProperties;
 import com.google.common.base.Stopwatch;
 import org.quartz.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
-import javax.naming.spi.StateFactory;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -21,7 +25,8 @@ import java.util.concurrent.TimeUnit;
 @DisallowConcurrentExecution
 public class ScheduledJob implements Job {
 
-    private static final Logger logger = LoggerFactory.getLogger(TaskSchedulePlugin.class);
+    //private static final Logger logger = LoggerFactory.getLogger(TaskSchedulePlugin.class);
+    private static final Logger logger = LoggerFactory.getLogger("container.scheduled.task");
 
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
@@ -34,17 +39,21 @@ public class ScheduledJob implements Job {
 //            logger.info("--定时任务({}:{})不是Master，跳过--", serviceName, versionName);
 //            return;
 //        }
-
         Stopwatch stopwatch = Stopwatch.createStarted();
+        /**
+         * 添加sessionTid
+         */
+        InvocationContext invocationContext = InvocationContextImpl.Factory.currentInstance();
+        invocationContext.sessionTid(DapengUtil.generateTid());
+        MDC.put(SoaSystemEnvProperties.KEY_LOGGER_SESSION_TID, invocationContext.sessionTid().orElse("0"));
+
+
         logger.info("定时任务({})开始执行", context.getJobDetail().getKey().getName());
         Map<ProcessorKey, SoaServiceDefinition<?>> processorMap = ContainerFactory.getContainer().getServiceProcessors();
         SoaServiceDefinition soaServiceDefinition = processorMap.get(new ProcessorKey(serviceName, versionName));
-
 //        SoaProcessFunction<Object, Object, Object, ? extends TCommonBeanSerializer<Object>, ? extends TCommonBeanSerializer<Object>> soaProcessFunction =
 //                (SoaProcessFunction<Object, Object, Object, ? extends TCommonBeanSerializer<Object>, ? extends TCommonBeanSerializer<Object>>) data.get("function");
-
         Object iface = data.get("iface");
-
         try {
             if (soaServiceDefinition.isAsync) {
                 SoaFunctionDefinition.Async<Object, Object, Object> functionDefinition = (SoaFunctionDefinition.Async<Object, Object, Object>) data.get("function");
@@ -54,7 +63,9 @@ public class ScheduledJob implements Job {
                 functionDefinition.apply(iface, null);
             }
             logger.info("定时任务({})执行完成,cost({}ms)", context.getJobDetail().getKey().getName(), stopwatch.stop().elapsed(TimeUnit.MILLISECONDS));
+            MDC.remove(SoaSystemEnvProperties.KEY_LOGGER_SESSION_TID);
         } catch (Exception e) {
+            MDC.remove(SoaSystemEnvProperties.KEY_LOGGER_SESSION_TID);
             logger.error("定时任务({})执行异常,cost({}ms)", context.getJobDetail().getKey().getName(), stopwatch.stop().elapsed(TimeUnit.MILLISECONDS));
             logger.error(e.getMessage(), e);
         }
