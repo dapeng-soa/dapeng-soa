1bef97156601067831702242a39cf1d32304b644
Ever <439674061@qq.com>
2018-07-22 20:47:24 +0800

    exit if enable freq control and shm.data init failed

--- dapeng-core/src/main/java/com/github/dapeng/core/SoaCode.java
+++ dapeng-core/src/main/java/com/github/dapeng/core/SoaCode.java
@@ -19,6 +19,7 @@ public enum SoaCode implements SoaBaseCodeInterface {
 
     // 服务端
     ServerUnKnown("Err-Core-500", "系统出错了!"),
+    ShmInitError("Err-Core-501", "限流模块初始化失败"),
     FreqLimited("Err-Core-502", "客户端已被限流"),
     NoMatchedService("Err-Core-504", "没有对应的服务或者没有对应的服务版本"),
     ServerNoMatchedMethod("Err-Core-505", "没有对应的方法"),
