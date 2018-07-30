35ea2d4effca3a0ea12dc80ec90dbee96efb061a
Ever <439674061@qq.com>
2018-07-30 14:39:32 +0800

    code review

--- dapeng-core/src/main/java/com/github/dapeng/core/lifecycyle/Lifecycle.java
+++ dapeng-core/src/main/java/com/github/dapeng/core/lifecycyle/Lifecycle.java
@@ -1,12 +1,16 @@
 package com.github.dapeng.core.lifecycyle;
 
 /**
- * @Author: hui
- * @Date: 2018/7/26 0026 11:21
- * @Description: 提供给业务的lifecycle接口，四种状态
+ * 提供给业务的lifecycle接口，四种状态
+ * @author hui
+ * @date 2018/7/26 0026 11:21
+ *
  */
 public interface Lifecycle {
 
+    /**
+     *
+     */
     void onStart();
 
     void onPause();
