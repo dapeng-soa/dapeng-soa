92451481aa9f931940968865923fabe179c62b90
leihuazhe <295482300@qq.com>
2018-09-27 10:32:46 +0800

    add commit

--- dapeng-router/src/test/java/TestRouterRuntimeList.java
+++ dapeng-router/src/test/java/TestRouterRuntimeList.java
@@ -19,10 +19,10 @@ import java.util.List;
 public class TestRouterRuntimeList {
 
 
-    RuntimeInstance runtimeInstance1 = new RuntimeInstance("com.maple.Uservice", "192.168.1.101", 9090, "1.0.0");
-    RuntimeInstance runtimeInstance2 = new RuntimeInstance("com.maple.Uservice", "192.168.1.102", 9091, "1.0.0");
-    RuntimeInstance runtimeInstance3 = new RuntimeInstance("com.maple.Uservice", "192.168.1.103", 9092, "1.0.0");
-    RuntimeInstance runtimeInstance4 = new RuntimeInstance("com.maple.Uservice", "192.168.1.104", 9093, "1.0.0");
+    private RuntimeInstance runtimeInstance1 = new RuntimeInstance("com.maple.Uservice", "192.168.1.101", 9090, "1.0.0");
+    private RuntimeInstance runtimeInstance2 = new RuntimeInstance("com.maple.Uservice", "192.168.1.102", 9091, "1.0.0");
+    private RuntimeInstance runtimeInstance3 = new RuntimeInstance("com.maple.Uservice", "192.168.1.103", 9092, "1.0.0");
+    private RuntimeInstance runtimeInstance4 = new RuntimeInstance("com.maple.Uservice", "192.168.1.104", 9093, "1.0.0");
 
 
     public List<RuntimeInstance> prepare(InvocationContextImpl ctx, List<Route> routes) {
