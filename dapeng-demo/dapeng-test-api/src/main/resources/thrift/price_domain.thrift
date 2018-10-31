1173006dba71fa9ccada6189faa7bc8eba1b57a9
梁斌斌 <bbliang@today36524.com.cn>
2018-10-31 11:26:32 +0800

    remove unused modules

--- dapeng-demo/dapeng-test-api/src/main/resources/thrift/price_domain.thrift
+++
@@ -1,8 +0,0 @@
-namespace java com.isuwang.soa.price.domain
-
-struct Price {
-
- 1: i32 orderId,
- 2: double price
-
-}
--- dapeng-tools-web/app/module/ServiceInfo.scala
+++
@@ -1,9 +0,0 @@
-package module
-
-/**
- *
- * @author Eric on 2016/7/15 10:27
- */
-class ServiceInfo(var serviceName:String ,var version : String, var port: String) {
-
-}
