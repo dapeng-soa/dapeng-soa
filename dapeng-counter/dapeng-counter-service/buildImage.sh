7abb6cc1050557d451d506d7dc563584de068f44
EmuuGrass <453394245@qq.com>
2019-02-19 09:59:42 +0800

    合并2.1.1

--- dapeng-counter/dapeng-counter-service/buildImage.sh
+++ dapeng-counter/dapeng-counter-service/buildImage.sh
@@ -2,5 +2,6 @@
 
 mvn clean package
 mv target/dapeng-counter-service docker/
-docker build -t dapengsoa/counter-service:2.1.1 docker/
+docker build -t docker.oa.isuwang.com:5000/system/counter-service:2.1.1 docker/
 rm -rf docker/dapeng-counter-service
+
