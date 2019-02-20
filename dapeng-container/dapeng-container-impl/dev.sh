bbb651eacb3609c0626de7f5f2bca876c751d572
bbliang <bbliang@today36524.com.cn>
2019-02-20 09:17:21 +0800

    fix dev.sh build bash and remove unused service plugin config

--- dapeng-container/dapeng-container-impl/dev.sh
+++ dapeng-container/dapeng-container-impl/dev.sh
@@ -31,4 +31,4 @@ cd ${enginedir}
 mvn clean package -Pdev
 
 cp ${enginedir}/target/dapeng-bootstrap.jar ${containerdir}/target/dapeng-container/bin/
-cp -rf ${soaparentdir}/dapeng-message/dapeng-scheduler/target/dapeng-scheduler ${containerdir}/target/dapeng-container/plugin/
+cp -rf ${soaparentdir}/dapeng-scheduler/target/dapeng-scheduler ${containerdir}/target/dapeng-container/plugin/
