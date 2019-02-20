caa4ad24b16ef72719e0f39469532f17674b2094
huyongjie <2573105190@qq.com>
2019-02-20 18:18:13 +0800

    scheduler message

---
+++ dapeng-scheduler/dapeng-scheduler-api/src/main/resources/thrift/task_result_enum.thrift
@@ -0,0 +1,14 @@
+namespace java com.today.api.scheduler.enums
+/**
+* 任务执行状态
+**/
+enum TaskStatusEnum {
+    /**
+    * 执行成功
+    **/
+    SUCCEED = 1,
+    /**
+    * 执行失败
+    **/
+    FAIL = 2
+}
