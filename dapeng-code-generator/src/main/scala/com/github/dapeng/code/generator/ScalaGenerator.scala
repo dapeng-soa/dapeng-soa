5433f80e513285ec101aef4aff56f78a90ba94bb
hui <fwu@today36524.com>
2019-05-28 09:25:11 +0800

    add enumList API at ScalaGenerator

--- dapeng-code-generator/src/main/scala/com/github/dapeng/code/generator/ScalaGenerator.scala
+++ dapeng-code-generator/src/main/scala/com/github/dapeng/code/generator/ScalaGenerator.scala
@@ -7,6 +7,7 @@ import com.github.dapeng.core.CustomConfig
 import com.github.dapeng.core.metadata.DataType.KIND
 import com.github.dapeng.core.metadata.TEnum.EnumItem
 import com.github.dapeng.core.metadata._
+import com.twitter.scrooge.backend.EnumTemplate
 
 import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
@@ -524,6 +525,11 @@ class ScalaGenerator extends CodeGenerator {
           case _ => UNDEFINED
         </block>
 
+      </block>
+
+      def enumList():List[Int] =
+      <block>
+      {toEnumItemArrayBuffer(enum.enumItems).map(_.value).mkString("List(",",",")")}
       </block>
 
     </block>
